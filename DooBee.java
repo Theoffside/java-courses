196c8d1a9a3ceeadff30534614c37cf00fb53d0e
Theoffside <exame@yandex.ru>
2018-07-11 19:37:04 +0700

    DooBee

---
+++ DooBee.java
@@ -0,0 +1,14 @@
+public class DooBee {
+	public static void main(String[] arg){
+		int x=1;
+		while (x<3) {
+			System.out.println("doo");
+			System.out.println("bee");
+			x=x+1;
+		}
+		if (x==3){
+			System.out.println("Do");
+		}
+
+	}
+}
